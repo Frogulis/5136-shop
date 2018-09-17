0d25a050b54e902e72413d0c59a4f8503b6d67be
yuki <robinscat@hotmail.com>
2018-09-17 19:25:36 +1000

    finish the user class
    Removed the type attribute;
    Added get/set methods for loggedIn attribute.

--- User.py
+++ User.py
@@ -1,11 +1,25 @@
 class User:
-    def __init__(self, id, name, type):
+    def __init__(self, id, name):
         self.id = id
         self.name = name
-        self.type = type
         self.loggedIn = False
 
     def getId(self):
         return self.id
+
+    def setId(self, newId):
+        self.id = newId
+
+    def getName(self):
+        return self.name
+
+    def setName(self, newName):
+        self.name = newName
+
+    def getLoggedIn(self):
+        return self.loggedIn
+
+    def setLoggedIn(self, newStatus):
+        self.loggedIn = newStatus
     
 
