b0832bdeb55b8c30c296a5d029edd510a5d56b26
Frogulis <jamierhoffmann@gmail.com>
2018-09-17 19:33:00 +1000

    Rename User, Customer to UserAccount, CustomerAccount

--- Customer.py
+++
@@ -1,7 +0,0 @@
-import User
-class Customer(User):
-    def __init__(self,phoneNumber, address, shoppingCart):
-        self.phoneNumber = phoneNumber
-        self.address = address
-        self.shoppingCart = shoppingCart
-        self.balance = 0
