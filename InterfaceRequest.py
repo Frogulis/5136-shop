46b2b33048139c221d8d1c2beaacf218c6a0ced4
Frogulis <jamierhoffmann@gmail.com>
2018-09-19 16:15:56 +1000

    Final 2 display types
    - also added writeLine to UserInterface in case
    - added aux field to existing displays for extra info

--- InterfaceRequest.py
+++ InterfaceRequest.py
@@ -17,43 +17,55 @@ class InterfaceRequest:
 			raise TypeError("field '" + str(field) + "' not in request")
 
 	@staticmethod
-	def getLIST_DISPLAY(title, list_items, option_string):
+	def getLIST_DISPLAY(title, list_items, option_string, aux=None):
 		my_dict = {}
 		my_dict['type'] = 'LIST_DISPLAY'
 		my_dict['title'] = title
 		my_dict['list_items'] = list_items
+		my_dict['aux'] = aux
 		my_dict['option_string'] = option_string
 		my_dict['single_input'] = ''
 		return InterfaceRequest(my_dict)
 
 	@staticmethod
-	def getFORM_DISPLAY(title, fields):
+	def getFORM_DISPLAY(title, fields, aux=None):
 		my_dict = {}
 		my_dict['type'] = 'FORM_DISPLAY'
 		my_dict['title'] = title
 		my_dict['fields'] = fields
+		my_dict['aux'] = aux
 		my_dict['inputs'] = []
 		return InterfaceRequest(my_dict)
 
 	@staticmethod
-	def getCONFIRM_DISPLAY(title, msg):
+	def getCONFIRM_DISPLAY(title, msg, aux=None):
 		my_dict = {}
 		my_dict['type'] = 'CONFIRM_DISPLAY'
 		my_dict['title'] = title
 		my_dict['msg'] = msg
+		my_dict['aux'] = aux
 		my_dict['response'] = ''
 		return InterfaceRequest(my_dict)
 
 	@staticmethod
-	def getITEM_DISPLAY(title, list_items, option_string):
-		output = InterfaceRequest.getLIST_DISPLAY(title, list_items, option_string)
+	def getITEM_DISPLAY(title, list_items, option_string, aux=None):
+		output = InterfaceRequest.getLIST_DISPLAY(title, list_items, option_string, aux)
 		output.setField('type', 'ITEM_DISPLAY')
 		return output
 
 if __name__ == '__main__':
-	ir = InterfaceRequest.getLIST_DISPLAY('Search results', [('Bananas ea', '$1.00'), ('Bananas kg', '$10.00')], 'X to exit, C to continue')
+	ir = InterfaceRequest.getLIST_DISPLAY('Search results',
+		[('Bananas ea', '$1.00'), ('Bananas kg', '$10.00')],
+		'X to exit, C to continue',
+		'Total 2 items')
 	ir = UserInterface.display(ir)
 	print('You said,', ir.getField('single_input'))
-	ir2 = InterfaceRequest.getCONFIRM_DISPLAY('Confirm whateer', 'Please confirm that you wish to proceed with the transaction')
+	ir2 = InterfaceRequest.getCONFIRM_DISPLAY('Confirm whateer', 'Please confirm that you wish to proceed with the transaction', 'skjfhsdfjhdsk')
 	ir2 = UserInterface.display(ir2)
 	print('You said,', ir2.getField('response'))
+	ir3 = InterfaceRequest.getFORM_DISPLAY('Registration: Please enter your details', [('Name', 'string'), ('Age', 'int')])
+	ir3 = UserInterface.display(ir3)
+	print('Responses were', ir3.getField('inputs'))
+	ir4 = InterfaceRequest.getITEM_DISPLAY('Customer', [('Name', 'Jamie Hoffmann'), ('Age', 23), ('Course', 'MIT')], 'X to exit, E to edit')
+	ir4 = UserInterface.display(ir4)
+	print('You said,', ir4.getField('single_input'))
--- UserInterface.py
+++ UserInterface.py
@@ -1,10 +1,19 @@
 class UserInterface:
 	@classmethod
+	def writeLine(c, s):
+		c._printWhitespace(s)
+
+	@classmethod
 	def display(c, request):
-		if request.getField('type') == 'LIST_DISPLAY':
+		t = request.getField('type')
+		if t == 'LIST_DISPLAY':
 			return c._displayList(request)
-		elif request.getField('type') == 'CONFIRM_DISPLAY':
+		elif t == 'CONFIRM_DISPLAY':
 			return c._displayConfirm(request)
+		elif t == 'FORM_DISPLAY':
+			return c._displayForm(request)
+		elif t == 'ITEM_DISPLAY':
+			return c._displayItem(request)
 		else:
 			return c._displayError(request)
 
@@ -14,6 +23,8 @@ class UserInterface:
 		for item in request.getField('list_items'):
 			c._printWhitespace(item[0], 2)
 			c._printWhitespace(item[1], 4)
+		if request.getField('aux') is not None:
+			c._printWhitespace(request.getField('aux'), 2)
 		c._printWhitespace(request.getField('option_string'))
 		i = input('Please input option: ')
 		request.setField('single_input', i)
@@ -23,8 +34,26 @@ class UserInterface:
 	def _displayConfirm(c, request):
 		c._printWhitespace(request.getField('title'))
 		c._printWhitespace(request.getField('msg'), 2)
+		if request.getField('aux') is not None:
+			c._printWhitespace(request.getField('aux'), 2)
 		i = input('y/n or blank to cancel: ')
 		request.setField('response', i)
+		return request
+
+	@classmethod
+	def _displayItem(c, request):
+		return c._displayList(request)
+
+	@classmethod
+	def _displayForm(c, request):
+		c._printWhitespace(request.getField('title'))
+		inputs = []
+		for item in request.getField('fields'):
+			c._printWhitespace(item[0], 2)
+			inputs.append(input())
+		request.setField('inputs', inputs)
+		if request.getField('aux') is not None:
+			c._printWhitespace(request.getField('aux'), 2)
 		return request
 
 	@classmethod
