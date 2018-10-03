1684e69cc9ae61604215710d2f58b620c3c8a8db
Frogulis <jamierhoffmann@gmail.com>
2018-10-03 17:03:02 +1000

    FileAccessor interface

---
+++ FileAccessor.py
@@ -0,0 +1,22 @@
+class FileAccessor:
+    def __init__(self, filename):
+        pass
+
+    def readStoreFromFile(self):
+        """Reads a store from file
+
+        Returns a Store object with all entities populated.
+        """
+        pass
+
+    def writeStoreToFile(self, store):
+        """Writes a store to file
+
+        Writes the given Store object to the file.
+        """
+        pass
+
+    def writeString(self, s):
+        """Wrapper for file access code
+        """
+        pass
