6a10694d80f92fb5398ba8ec9eca65096e3f4ab0
yuki <robinscat@hotmail.com>
2018-10-04 20:20:57 +1000

    update store
    update store class

--- Store.py
+++ Store.py
@@ -1,19 +1,86 @@
 import Order
-import Product
+from Product import Product
+from CustomerAccount import CustomerAccount
+
+import datetime
 
 
 class Store:
-    def __init__(self, products, customers, owner, orderHistory):
-        self.products = products
-        self.customers = customers
-        self.owner = owner
-        self.orderHistory = orderHistory
+    def __init__(self):
+        # readfile()
+        self.products = []
+        self.customers = []
+        self.owner = []
+        self.orderHistory = {}
+
+    def addProduct(self, name, unit, originalPrice, source, shelfLife):
+        id = self.generateNewProductId()
+        newProduct = Product(id, name, unit, originalPrice, source, shelfLife)
+        self.products.append(newProduct)
+
+    def addCustomer(self, password, name, phoneNum, address):
+        id = self.generateNewCustomerId()
+        newCustomer = CustomerAccount(id, password, name, phoneNum, address)
+        self.customers.append(newCustomer)
+
+    def generateNewCustomerId(self):
+        if len(self.customers) == 0:
+            return str(1)
+        return str(int(self.customers[-1].getId())+1)
+
+
+    def editProductName(self, productId, newName):
+        productToBeEdited = self.getProduct(productId)
+        productToBeEdited.setName(newName)
+
+    def editProductUnit(self, productId, newUnit):
+        productToBeEdited = self.getProduct(productId)
+        productToBeEdited.setUnit(newUnit)
+
+    def editProductOriginalPrice(self, productId, newPrice):
+        productToBeEdited = self.getProduct(productId)
+        productToBeEdited.setOriginalPrice(newPrice)
+
+    def editProductSource(self, productId, newSource):
+        productToBeEdited = self.getProduct(productId)
+        productToBeEdited.setSource(newSource)
+
+    def editProductShelfLife(self, productId, newShelfLife):
+        productToBeEdited = self.getProduct(productId)
+        productToBeEdited.setShelfLife(newShelfLife)
+
+    # Please make sure the current user is the owner, validation needed.
+    def getExpiredProducts(self):
+        expiredProducts = []
+        for product in self.products:
+            for batch in product.getBatches():
+                if batch.getExpiryDate() < datetime.datetime.now().date():
+                    expiredProducts.append(batch)
+        return expiredProducts
+
+    def generateNewProductId(self):
+        if len(self.products) == 0:
+            return str(1)
+        return str(int(self.products[-1].getId())+1)
+
+    def getProduct(self, productId):
+        for product in self.products:
+            if product.getId() == productId:
+                return product
+        raise Exception("Product does not exist.")
+
 
     def getProducts(self):
-        return self.products;
+        return self.products
 
     def setProducts(self, productList):
         self.products = productList
+
+    def getCustomer(self, customerId):
+        for customer in self.customers:
+            if customer.getId() == customerId:
+                return customer
+        raise Exception("Customer does not exist.")
 
     def getCustomers(self):
         return self.customers
@@ -33,19 +100,38 @@ class Store:
     def setOrderHistory(self, allOrders):
         self.orderHistory = allOrders
 
-    def getParticularOrder(self, orderID):
-        orders = self.getOrders()
-        return orders[orderID]
+    def getParticularOrder(self, customerId, orderId):
+        orders = self.orderHistory[customerId]
+        for order in orders:
+            if order.getOrderId() == orderId:
+                return order
+        raise Exception("Order does not exist.")
 
-    def getOrders(self):
-        return Order()
 
-    def searchProduct(self, productID):
-        products = self.getProduct()
-        product = products[productID]
+    def searchProductByName(self, keyword):  # TODO
+        keyword.lower()
+        matchingProducts = []
+        for product in self.products:
+            searchingProductName = product.getName().lower()
+            if keyword in searchingProductName:
+                matchingProducts.append(product)
+        return matchingProducts
 
-    def getProduct(self):
-        return Product()
+    def removeProduct(self, productId):
+        try:
+            productToBeRemoved = self.getProduct(productId)
+            productIndex = self.products.index(productToBeRemoved)
+            del self.products[productIndex]
+        except Exception:
+            raise Exception("Failed to remove product.")
+
+    def removeCustomer(self, customerId):
+        try:
+            customerToBeRemoved = self.getCustomer(customerId)
+            customerIndex = self.customers.index(customerToBeRemoved)
+            del self.customers[customerIndex]
+        except Exception:
+            raise Exception("Failed to remove customer.")
 
 
 if __name__ == '__main__':
@@ -56,6 +142,10 @@ if __name__ == '__main__':
     orderHistory = [{"C1000": "Order1"}, {"C1002": "Order2"}]
 
     # create object
-    c = Store(products, customers, owner, orderHistory)
+    # c = Store(products, customers, owner, orderHistory)
 
-    print (c.getParticularOrder('Order3'));
+    s=Store()
+    s.addProduct('apple', 'bag', 2.5, 'china', 9)
+    print(1, s.getProducts()[0].getId())
+    s.editProductName('1', 'banana')
+    s.editProductOriginalPrice('1','er')
