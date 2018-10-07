5b883cb5afae0432801908ae235e8588aa0ae0ca
Mich-L <Mich-L@github.com>
2018-10-07 23:35:41 +1100

    tested methods

--- ShoppingCart.py
+++ ShoppingCart.py
@@ -5,21 +5,22 @@ class ShoppingCart:
     def __init__(self):
         self.productsInCart = []
 
-    def addToShoppingCart(self, itemToBeAdded):
-        self.productsInCart.append(itemToBeAdded)
+    #def addToShoppingCart(self, itemToBeAdded):
+    #    self.productsInCart.append(itemToBeAdded)
 
     def addToShoppingCart(self, productId, actualPrice = 0.00, quantity = 0.00):
-        self._convertToFloat(actualPrice)
-        self._convertToFloat(quantity)
-        newTuple = [productId,actualPrice,quantity]
+        #self._convertToFloat(actualPrice)
+        #self._convertToFloat(quantity)
+        newTuple = [productId,round(actualPrice,2),round(quantity,2)]
         self.productsInCart.append(newTuple)
 
-    def adjustQuantity(self, productId, actualPrice = 0.00, newQuantity = 0.00):
-        actualPrice = self._convertToFloat(actualPrice)
+    # def adjustQuantity(self, productId, actualPrice = 0.00, newQuantity = 0.00):
+    def adjustQuantity(self, productId, newQuantity):
+        # actualPrice = self._convertToFloat(actualPrice)
         newQuantity = self._convertToFloat(newQuantity)
         found = False
         for item in self.productsInCart:
-            if item[0] == productId and item[1] == actualPrice:
+            if item[0] == productId:      # and item[1] == actualPrice:
                 item[2] = newQuantity
                 found = True
         if not found:
@@ -32,17 +33,18 @@ class ShoppingCart:
             raise Exception("Parameter should be a float or integer.")
         return floated
 
-    def deleteFromShoppingCart(self, itemToBeDeleted):
-        for i in range(len(self.productsInCart)):
-            if self.productsInCart[i] == itemToBeDeleted:
-                del self.productsInCart[i]
+    # def deleteFromShoppingCart(self, itemToBeDeleted):
+    #    for i in range(len(self.productsInCart)):
+    #        if self.productsInCart[i] == itemToBeDeleted:
+    #            del self.productsInCart[i]
 
-    def deleteFromShoppingCart(self, productId, actualPrice = 0.00):
-        actualPrice = self._convertToFloat(actualPrice)
+    # def deleteFromShoppingCart(self, productId, actualPrice = 0.00):
+    #    actualPrice = self._convertToFloat(actualPrice)
+    def deleteFromShoppingCart(self, productId):
         found = False
-        for i in range(len(self.productsInCart)):
+        for i in range(len(self.productsInCart) - 1):
             item = self.productsInCart[i]
-            if item[0] == productId and item[1] == actualPrice:
+            if item[0] == productId:    # and item[1] == actualPrice:
                 del self.productsInCart[i]
                 found = True
         if not found:
@@ -64,7 +66,28 @@ class ShoppingCart:
         for item in self.productsInCart:
             singlePrice = item[1] * item[2]
             totalPrice += singlePrice
-        return totalPrice
+        return round(totalPrice,2)
 
     def setProductsInCart(self, newList):
         self.productsInCart = newList
+
+# ignore - these are test calls   (ML)
+if __name__ == '__main__':
+    # productsInCart = [('20010', 3.80, 2), ('20011', 2.90, 4)]
+    sCart = ShoppingCart()
+    # sCart.setProductsInCart([['20001', 1.50, 4], ['20013', 3.89, 10]])
+    sCart.addToShoppingCart('20001', 1.50, 4)
+    sCart.addToShoppingCart('20013', 3.89, 10)
+    print(sCart.getProductsInCart())
+    sCart.addToShoppingCart('20010', 3.80, 2)
+    print(sCart.getProductsInCart())
+    sCart.addToShoppingCart('200230', 4.99, 1.8943536)
+    print(sCart.getProductsInCart())
+    sCart.adjustQuantity('20010',5.5)
+    print(sCart.getProductsInCart())
+    sCart.deleteFromShoppingCart('20001')
+    print(sCart.getProductsInCart())
+    print(sCart.getProductInCart('200230'))
+    print(sCart.getTotalPrice())
+
+
