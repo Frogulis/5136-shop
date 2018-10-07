5d20fa7b954c74194d23b0de628ca50c65759d08
Jamie Hoffmann <jamsarhoff@hotmail.com>
2018-10-07 18:04:44 +1100

    changed references to tuples in ShoppingCart to references to items and lists

--- ShoppingCart.py
+++ ShoppingCart.py
@@ -1,25 +1,28 @@
 
 class ShoppingCart:
 
-    # tuple = (productId, actualPrice, quantity)
+    # item = [productId, actualPrice, quantity]
     def __init__(self):
         self.productsInCart = []
 
-    def addToShoppingCart(self, tupleToBeAdded):
-        self.productsInCart.append(tupleToBeAdded)
+    def addToShoppingCart(self, itemToBeAdded):
+        self.productsInCart.append(itemToBeAdded)
 
     def addToShoppingCart(self, productId, actualPrice = 0.00, quantity = 0.00):
         self._convertToFloat(actualPrice)
         self._convertToFloat(quantity)
-        newTuple = (productId,actualPrice,quantity)
+        newTuple = [productId,actualPrice,quantity]
         self.productsInCart.append(newTuple)
 
     def adjustQuantity(self, productId, actualPrice = 0.00, newQuantity = 0.00):
         actualPrice = self._convertToFloat(actualPrice)
         newQuantity = self._convertToFloat(newQuantity)
-        for tuple in self.productsInCart:
-            if tuple[0] == productId and tuple[1] == actualPrice:
-                tuple[2] = newQuantity
+        found = False
+        for item in self.productsInCart:
+            if item[0] == productId and item[1] == actualPrice:
+                item[2] = newQuantity
+                found = True
+        if not found:
             raise Exception("Either Product Id or Actual Price is invalid.")
 
     def _convertToFloat(self, integer):
@@ -34,18 +37,19 @@ class ShoppingCart:
 
     def deleteFromShoppingCart(self, productId, actualPrice = 0.00):
         actualPrice = self._convertToFloat(actualPrice)
-        for tuple in self.productsInCart:
-            if tuple[0] == productId and tuple[1] == actualPrice:
-                del self.productsInCart[tuple]
-            else:
-                raise Exception("Product Id or Actual Price is invalid.")
+        found = False
+        for item in self.productsInCart:
+            if item[0] == productId and item[1] == actualPrice:
+                del self.productsInCart[item]
+        if not found:
+            raise Exception("Product Id or Actual Price is invalid.")
 
     # To get one particular product tuple
     def getProductInCart(self, productId):
-        for tuple in self.productsInCart:
-            if productId == tuple[0]:
-                return tuple
-            raise Exception("Product Id is invalid.")
+        for item in self.productsInCart:
+            if productId == item[0]:
+                return item
+        raise Exception("Product Id is invalid.")
 
     def getProductsInCart(self):
         return self.productsInCart
@@ -53,10 +57,10 @@ class ShoppingCart:
     # To get the total price of the shopping cart.
     def getTotalPrice(self):
         totalPrice = 0.00
-        for tuple in self.productsInCart:
-            singlePrice = tuple[1] * tuple[2]
+        for item in self.productsInCart:
+            singlePrice = item[1] * item[2]
             totalPrice += singlePrice
         return totalPrice
 
-    def setProductsInCart(self,newTuple):
-        self.productsInCart = newTuple
+    def setProductsInCart(self, newList):
+        self.productsInCart = newList
