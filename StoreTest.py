53820c6339d21355345c3617e8684ccf583f0849
Jamie Hoffmann <jamsarhoff@hotmail.com>
2018-10-07 16:48:32 +1100

    few more tests

--- StoreTest.py
+++ StoreTest.py
@@ -4,6 +4,7 @@ import datetime
 from Store import Store
 from Product import Product
 from Batch import Batch
+from CustomerAccount import CustomerAccount
 
 class StoreTest(unittest.TestCase):
     def setUp(self):
@@ -14,19 +15,35 @@ class StoreTest(unittest.TestCase):
         self.p1.batches.append(Batch("3", 5.00, 10, datetime.date.today() - datetime.timedelta(days = 12), 14))
         self.p1.updateDiscount()
 
-    def test1(self):
+    def test1(self): #addProduct
         self.s1.addProduct("Cheese", "kg", 10.00, "France", 50)
         self.assertEqual(len(self.s1.products), 1)
         self.assertEqual(self.s1.products[-1].id, "1")
 
     def test2(self):
-        self.s1.products = [self.p1]
+        self.s1.setProducts([self.p1])
         self.s1.addProduct("Cheese", "kg", 10.00, "France", 50)
         self.assertEqual(len(self.s1.products), 2)
         self.assertEqual(self.s1.products[-1].id, "2")
 
-    def test3(self):
-        pass
+    def test3(self): #addCustomer
+        self.s1.addCustomer('doggo1', 'Jerj Clooners', '0312345678', '123 Hollywoo Bvd')
+        self.assertEqual(len(self.s1.customers), 1)
+        self.assertEqual(self.s1.getCustomer("1").getName(), 'Jerj Clooners')
+
+    def test4(self): #getOrderHistoryAsList
+        each_l = 5
+        no_c = 10
+        orderHistory = {str(i): [None] * each_l for i in range(1,no_c + 1)}
+        self.s1.setOrderHistory(orderHistory)
+        self.assertEqual(len(self.s1.getOrderHistoryAsList()), each_l * no_c)
+
+    def test5(self):
+        self.assertEqual(len(self.s1.getOrderHistoryAsList()), 0)
+
+    def test6(self):
+        
+
 
 if __name__ == '__main__':
     test = StoreTest()
