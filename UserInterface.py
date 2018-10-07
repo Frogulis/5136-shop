0a229b29e1b55aa8328c5bc22a02534200e6649e
Mich-L <Mich-L@github.com>
2018-10-07 19:09:28 +1100

    -

--- UserInterface.py
+++ UserInterface.py
@@ -1,5 +1,5 @@
 class UserInterface:
-	"""User interface boundary class
+    """User interface boundary class
 
 	Users of this class only need to worry about these methods:
 
@@ -34,85 +34,86 @@ class UserInterface:
 	
 	"""
 
-	@classmethod
-	def writeLine(c, s):
-		c._printWhitespace(s)
+    @classmethod
+    def writeLine(c, s):
+        c._printWhitespace(s)
 
-	@classmethod
-	def displayList(c, title, list_items, option_string, aux=None):
-		c._printWhitespace(title)
-		for item in list_items:
-			c._printWhitespace(item[0], 2)
-			c._printWhitespace(item[1], 4)
-		if aux is not None:
-			c._printWhitespace(aux, 2)
-		c._printWhitespace(option_string)
-		i = input('Please input option: ')
-		return i
+    @classmethod
+    def displayList(c, title, list_items, option_string, aux=None):
+        c._printWhitespace(title)
+        for item in list_items:
+            c._printWhitespace(item[0], 2)
+            c._printWhitespace(item[1], 4)
+        if aux is not None:
+            c._printWhitespace(aux, 2)
+        c._printWhitespace(option_string)
+        i = input('Please input option: ')
+        return i
 
-	@classmethod
-	def displayConfirm(c, title, msg, aux=None):
-		c._printWhitespace(title)
-		c._printWhitespace(msg, 2)
-		if aux is not None:
-			c._printWhitespace(aux, 2)
-		i = input('y/n or blank to cancel: ')
-		return i
+    @classmethod
+    def displayConfirm(c, title, msg, aux=None):
+        c._printWhitespace(title)
+        c._printWhitespace(msg, 2)
+        if aux is not None:
+            c._printWhitespace(aux, 2)
+        i = input('y/n or blank to cancel: ')
+        return i
 
-	@classmethod
-	def displayItem(c, title, list_items, option_string, aux=None):
-		return c.displayList(title, list_items, option_string, aux)
+    @classmethod
+    def displayItem(c, title, list_items, option_string, aux=None):
+        return c.displayList(title, list_items, option_string, aux)
 
-	@classmethod
-	def displayForm(c, title, fields, aux=None):
-		c._printWhitespace(title)
-		inputs = []
-		for item in fields:
-			c._printWhitespace(item[0], 2)
-			inputs.append(input())
-		if aux is not None:
-			c._printWhitespace(aux, 2)
-		return inputs
+    @classmethod
+    def displayForm(c, title, fields, aux=None):
+        c._printWhitespace(title)
+        inputs = []
+        for item in fields:
+            c._printWhitespace(item[0], 2)
+            inputs.append(input())
+        if aux is not None:
+            c._printWhitespace(aux, 2)
+        return inputs
 
-	@classmethod
-	def displayError(c, string):
-		print("Error: ", string)
-		return None
+    @classmethod
+    def displayError(c, string):
+        print("Error: ", string)
+        return None
 
-	@classmethod
-	def _printWhitespace(c, s, no_spaces=0, line_end='\n'):
-		for i in range(no_spaces):
-			print(' ', end='')
-		print(s, end=line_end)
+    @classmethod
+    def _printWhitespace(c, s, no_spaces=0, line_end='\n'):
+        for i in range(no_spaces):
+            print(' ', end='')
+        print(s, end=line_end)
+
 
 if __name__ == '__main__':
-	#testing
-	t1 = "A title"
-	t2 = \
-"A veeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee\
+    # testing
+    t1 = "A title"
+    t2 = \
+        "A veeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee\
 eeeeeery long tiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiitle"
-	li1 = [('Banana', 'Unit: ea, Source: QLD, Expiry date: 22/22/22, Price: $1'),
-			('Apple', 'Unit: ea, Source: SA, Expiry date: 22/22/22, Price: $1.50'),
-			('Potato', 'Unit: kg, Source: VIC, Expiry date: 22/22/22, Price: $10'),
-			('Marijuana', 'Unit: kg, Source: Himalayas, Expiry date: 22/22/22, Price: $5000')]
-	li2 = [('Banalongboynananananananananananananananananananananananananana\
+    li1 = [('Banana', 'Unit: ea, Source: QLD, Expiry date: 22/22/22, Price: $1'),
+           ('Apple', 'Unit: ea, Source: SA, Expiry date: 22/22/22, Price: $1.50'),
+           ('Potato', 'Unit: kg, Source: VIC, Expiry date: 22/22/22, Price: $10'),
+           ('Marijuana', 'Unit: kg, Source: Himalayas, Expiry date: 22/22/22, Price: $5000')]
+    li2 = [('Banalongboynananananananananananananananananananananananananana\
 		nananananananana', 'Unit: ea, Source: QLD, Expiry date: 22/22/22, Price: $1'),
-			('Apple', 'Unit: ea, Source: SA, Expiry date: 22/22/22, Price: $1.50'),
-			('Potato', 'Unit: kg, Source: VIC, Expiry date: 22/22/22, Price: $10')]
-	f1 = [('Name', 'string'), ('Address', 'string'), ('Age', 'int')]
-	f2 = [('thing', 'string') for _ in range(10)]
-	o1 = 'X to exit, C to continue, W to whatever'
+           ('Apple', 'Unit: ea, Source: SA, Expiry date: 22/22/22, Price: $1.50'),
+           ('Potato', 'Unit: kg, Source: VIC, Expiry date: 22/22/22, Price: $10')]
+    f1 = [('Name', 'string'), ('Address', 'string'), ('Age', 'int')]
+    f2 = [('thing', 'string') for _ in range(10)]
+    o1 = 'X to exit, C to continue, W to whatever'
 
-	choice = input("which test u want:\n1. list\n2. form\n3. confirm")
-	if choice == '1':
-		print("u input: ", UserInterface.displayList(t1, li1, o1))
-		print("u input: ", UserInterface.displayList(t2, li2, o1))
-		print("u input: ", UserInterface.displayList(t1, li1, o1, 'some aux string'))
-	elif choice == '2':
-		print("u input: ", UserInterface.displayForm(t1, f1))
-		print("u input: ", UserInterface.displayForm(t2, f2, 'some aux'))
-	elif choice == '3':
-		print("u input: ", UserInterface.displayConfirm('Confirm purchase',
-			'Are you sure you\'d like to proceed?'))
-	else:
-		print("invalid choice")
+    choice = input("which test u want:\n1. list\n2. form\n3. confirm\n4. browse products")
+    if choice == '1':
+        print("u input: ", UserInterface.displayList(t1, li1, o1))
+        print("u input: ", UserInterface.displayList(t2, li2, o1))
+        print("u input: ", UserInterface.displayList(t1, li1, o1, 'some aux string'))
+    elif choice == '2':
+        print("u input: ", UserInterface.displayForm(t1, f1))
+        print("u input: ", UserInterface.displayForm(t2, f2, 'some aux'))
+    elif choice == '3':
+        print("u input: ", UserInterface.displayConfirm('Confirm purchase'
+                                                        , 'Are you sure you\'d like to proceed?'))
+    else:
+        print("invalid choice")
